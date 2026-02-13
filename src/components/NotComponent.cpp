79d7c41d50f468cca5686d89e385e785fc74c5f0
Hugo ARNAL <hugo@hugoarnal.com>
2026-02-13 22:00:40 +0100

    fix: Not undefined input

--- src/components/NotComponent.cpp
+++ src/components/NotComponent.cpp
@@ -1,4 +1,5 @@
 #include "components/NotComponent.hpp"
+#include "Tristate.hpp"
 #include "components/AComponent.hpp"
 
 nts::NotComponent::NotComponent() : AComponent()
@@ -12,9 +13,16 @@ nts::NotComponent::~NotComponent()
 nts::Tristate nts::NotComponent::compute(std::size_t pin)
 {
     switch (pin) {
-        case 2:
-            _state = static_cast<nts::Tristate>(!static_cast<bool>(getLink(1)));
+        case 2: {
+            nts::Tristate linkState = getLink(1);
+
+            if (linkState == Undefined) {
+                _state = Undefined;
+            } else {
+                _state = static_cast<nts::Tristate>(!static_cast<bool>(linkState));
+            }
             return _state;
+        }
         default:
             _state = getLink(pin);
             return _state;
